7351325fd5d0c88bb7272eb3b2ea0e8f443bbc84
JarvisCraft <mrjarviscraft@gmail.com>
2019-01-08 20:23:05 +0300

    Add QuestEvents class

---
+++ src/main/java/ru/questc/events/api/QuestEvents.java
@@ -0,0 +1,11 @@
+package ru.questc.events.api;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Static accessor of all QuestEvents components.
+ * Some of those may be unavailable so {@link java.util.Optional}s are used
+ */
+@UtilityClass
+public class QuestEvents {
+}
